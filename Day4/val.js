0477ee3a886113361f5cb52e6c3672c7d59a80ee
amaan27072003 <amaan27200307@gmail.com>
2024-10-08 15:36:32 +0530

    validation

---
+++ Day4/val.js
@@ -0,0 +1,8 @@
+function Validated(){
+    let username = document.getElementById("un").value;
+    let pass = document.getElementById("pass").value; 
+
+    if(username.length<10) 
+        return false;
+    return true;
+} 
