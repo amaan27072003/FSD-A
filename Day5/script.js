d97fe1c5f2f915a4bdca2ba4f2311bc1c5bad498
amaan27072003 <amaan27200307@gmail.com>
2024-10-15 15:40:49 +0530

    hello

---
+++ Day5/script.js
@@ -0,0 +1,19 @@
+// let x = confirm("are you sure to logout");
+// alert(x);
+
+// let v = prompt("enter your name","Rahul");
+// alert(v);
+
+// let disp = ()=>{
+//     console.log("welcome to arrow function in js");
+// }
+// disp();
+
+// let say =()=>console.log("arrow function");
+// say();
+
+let sum =(x,y)=>{
+    return x+y;    
+}
+let res = sum(12,32);
+console.log(res)
